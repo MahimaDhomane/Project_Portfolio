5dbac25239ab079c6028e3b6726d4a6e3c37940c
Shivani Malviya <83264561+Shivi2202@users.noreply.github.com>
2024-05-19 02:01:56 +0530

    Add files via upload

---
+++ scriptjs.js
@@ -0,0 +1,8 @@
+function toggleDetails(id) {
+    var details = document.getElementById(id);
+    if (details.style.display === "block") {
+        details.style.display = "none";
+    } else {
+        details.style.display = "block";
+    }
+}
